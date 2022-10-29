38becc71a643dc12e8fc8b2e39a2a23eab64cf9d
thales-bpl <thales.bplima@gmail.com>
2022-10-29 02:49:36 -0300

    fix: indent errors

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if (word == '' or word[low_index] != word[high_index]): return False
-    if (low_index == round(len(word)/2)): return True
+    if (word == '' or word[low_index] != word[high_index]):
+        return False
+
+    if (low_index == round(len(word)/2)):
+        return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,9 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
-    for user_schedule in permanence_period:
+    for usr_schedl in permanence_period:
         try:
-            if user_schedule[0] <= target_time <= user_schedule[1] and target_time is not None: counter += 1
+            if usr_schedl[0] <= target_time <= usr_schedl[1] and target_time is not None:
+                counter += 1
         except TypeError:
             return None
 
