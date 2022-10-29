fbaa3697cae17d4728e84864c2443700e6f0be79
thales-bpl <thales.bplima@gmail.com>
2022-10-29 02:16:23 -0300

    feat: study_schedule()

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+    for user_schedule in permanence_period:
+        try:
+            if user_schedule[0] <= target_time <= user_schedule[1] and target_time is not None: counter += 1
+        except TypeError:
+            return None
+
+    return counter
