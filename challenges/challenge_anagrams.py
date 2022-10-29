2d116b97c4799e2ec3a295e21395aaa05a4a50c6
thales-bpl <thales.bplima@gmail.com>
2022-10-29 05:19:01 -0300

    fix: lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,12 +7,13 @@ def is_anagram(first_word, second_word):  # complex: O(n)
     if (first_word == '' or second_word == ''):  # complex: O(1)
         return False
 
-    dupe_letter_index = second_word.find(first_word[0])  # complex: O(n)
-    if (dupe_letter_index == -1):
+    dupe_letter_idx = second_word.find(first_word[0])  # complex: O(n)
+    if (dupe_letter_idx == -1):
         return False
 
     first_word = first_word[1:]  # complex: O(n)
-    second_word = second_word[:dupe_letter_index] + second_word[dupe_letter_index + 1:]  # complex: O(n)
+    # complex L16 : O(n)
+    second_word = second_word[:dupe_letter_idx]+second_word[dupe_letter_idx + 1:]
 
     if (first_word == '' and second_word == ''):  # complex: O(1)
         return True
