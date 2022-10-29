f70155c14d26a3b0ad6d3ff462f389a4eceb83e4
thales-bpl <thales.bplima@gmail.com>
2022-10-29 02:56:51 -0300

    fix: indent

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,8 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
-    for usr_schedl in permanence_period:
+    for user in permanence_period:
         try:
-            if usr_schedl[0] <= target_time <= usr_schedl[1] and target_time is not None:
+            if user[0] <= target_time <= user[1] and target_time is not None:
                 counter += 1
         except TypeError:
             return None
