618b8f6b85b5cd0f92678f941bbe6e2378dd2b05
thales-bpl <thales.bplima@gmail.com>
2022-11-01 04:14:11 -0300

    feat: is_palindrome_iterative()

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,16 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+def is_palindrome_iterative(word):  # O(n/2)
+    checkpoint = round(len(word)/2)
+
+    for index in range(checkpoint):
+        testing_index = -1
+
+        if (word[index] == '' or
+            word[testing_index] == '' or
+            word[index] != word[testing_index]):
+            return False
+
+        testing_index -= 1
+        if (testing_index == -checkpoint):
+            break
+        
+    return True
