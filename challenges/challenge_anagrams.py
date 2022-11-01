72b37312c83108fc0a5c738fca7d464db5e8d46f
thales-bpl <thales.bplima@gmail.com>
2022-11-01 03:48:00 -0300

    feat: find_duplicate()

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -11,7 +11,7 @@ def is_anagram(fst_word, snd_word):  # complex: O(n)
     if (dupe_letter_idx == -1):
         return False
 
-    fst_word = fst_word[1:]  # complex: O(n)
+    fst_word = fst_word[1:]  # complex: O(k) = O(1)
     snd_word = snd_word[:dupe_letter_idx] + snd_word[dupe_letter_idx + 1:]
 
     if (fst_word == '' and snd_word == ''):  # complex: O(1)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,36 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
+def find_duplicate(array):
+    sorted_array = bubble_sort(array)  # O(n²)
+    n = len(array)
+    any_dupe_number = False
+    
+    for i in range(n - 1): # O(n - 1)
+        if type(sorted_array[i]) == str or sorted_array[i] < 0:
+            break
+
+        if sorted_array[i] == sorted_array[i + 1]:
+            dupe_number = sorted_array[i]
+            any_dupe_number = True
+
+        if any_dupe_number:
+            return dupe_number
+
+    return any_dupe_number
+
+
+def bubble_sort(array):  # O(n²)
+    # src: https://realpython.com
+    # /sorting-algorithms-python/#the-significance-of-time-complexity
+    n = len(array)
+
+    for i in range(n):
+        already_sorted = True
+
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                already_sorted = False
+
+        if already_sorted:
+            break
+
+    return array
