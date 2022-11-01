2d60fd8e3551bf6237ff184c14d76caad630b16b
thales-bpl <thales.bplima@gmail.com>
2022-11-01 17:56:12 -0300

    fix: palindromes_iteractive() loop

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,15 +3,14 @@ def is_palindrome_iterative(word):  # O(n/2)
         return False
 
     checkpoint = round(len(word)/2)
+    testing_index = -1
 
     for index in range(checkpoint):
-        testing_index = -1
-
         if (word[index] == '' or
                 word[testing_index] == '' or
                 word[index] != word[testing_index]):
             return False
 
         testing_index -= 1
-        
+
     return True
